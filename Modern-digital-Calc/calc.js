5ddad5d0e0982e9b4d5c7e762496eee9a9021df4
B Vijay <vijaydharmarambokoliya@gmail.com>
2026-02-09 15:04:44 +0530

    Completed v.0 of calc
    with Documentation
    -bugs
    -version
    -future updates

--- Modern-digital-Calc/calc.js
+++ Modern-digital-Calc/calc.js
@@ -2,14 +2,21 @@
 let buttons = document.querySelectorAll("li")
 let screen = document.querySelector(".display")
 
-let js_evaluatable = (string)=>{
-    return string = string.replace(/√\(/,"Math.sqrt(")
-                    .replace(/log\(/,"Math.log10(")
+let js_evaluatable = (string) => {
+
+    // √, log logic 
+    string = string.replace(/√\(/g, "Math.sqrt(")
+        .replace(/(\d)\(/g, "$1*(")
+        .replace(/log\(/g, "Math.log10(")
+        .replace(/\)(\d)/g, ")*$1")
+        .replace(/\)\(/g, ")*(");
+
+    return string
 }
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        if (screen.textContent.trim() === "Welcome"||screen.textContent.trim()==="Syntax Error") {
+        if (screen.textContent.trim() === "Welcome" || screen.textContent.trim() === "Syntax Error") {
             screen.textContent = ""
         }
         switch (button.textContent) {
@@ -18,26 +25,24 @@ buttons.forEach((button) => {
                 break;
             case "=":
                 try {
+                    console.log("Original:", screen.textContent);
                     let expresion = js_evaluatable(screen.textContent)
+                    console.log("Converted:", expresion);
                     let result = new Function('return ' + expresion)()
+                    console.log("Result:", result);
                     screen.textContent = screen.textContent != "" ? result : "";
                 }
                 catch {
                     screen.textContent = "Syntax Error"
                 }
                 break;
-            case "(":
-                let last_character = screen.textContent.trim().slice(-1)
-                if (/[0-9)]/.test(last_character)) {
-                    screen.textContent += "*"
-                }
-                screen.textContent += "("
-                break;
             case "Del":
                 let length = screen.textContent.length
-                screen.textContent[length-2]=="g"?
-                screen.textContent =screen.textContent.slice(0,-4):
-                screen.textContent = screen.textContent.slice(0, -1);
+                screen.textContent[length - 2] == "g" ?
+                    screen.textContent = screen.textContent.slice(0, -4) :
+                    screen.textContent[length - 2] == "√" ?
+                        screen.textContent = screen.textContent.slice(0, -2) :
+                        screen.textContent = screen.textContent.slice(0, -1);
                 break;
             case "√":
                 screen.textContent += "√("
