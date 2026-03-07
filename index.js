f787c654c02c0440cb96c7c07c8ae6781b42ec87
Vijay <vijayredev@gmail.com>
2026-03-07 16:05:30 +0530

    secure env vars

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("Beggining of backend proj.");
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env"
+})
+
+let myusername = process.env.database
+
+console.log("value is :",myusername);
+
 console.log("Beggining of backend proj.");
